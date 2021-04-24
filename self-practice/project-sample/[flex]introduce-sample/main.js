0769905517ee048ed0015344bd3c4e4fb29d2993
parkhanseong <hs88610@gmail.com>
2021-04-24 20:02:08 +0900

    JS: navbar scroll smooth

--- self-practice/project-sample/[flex]introduce-sample/main.js
+++ self-practice/project-sample/[flex]introduce-sample/main.js
@@ -3,7 +3,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-
 // navbar가 상단일 때 투명처리
 document.addEventListener('scroll', () => {
     if(window.scrollY > navbarHeight){
@@ -12,3 +11,20 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// navbar 눌렀을때 스크롤 이동시키기
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener('click', (event) => {
+    console.log(event);
+    console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if ( link == null ) {
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+
+})
