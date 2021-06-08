2000832510f4007a89b2247d212d252f9d24be78
parkhanseong <hs88610@gmail.com>
2021-06-08 19:40:11 +0900

    와우, 스와이퍼, 풀페이지 라이브 추가

---
+++ self-practice/풀페이지js/script.js
@@ -0,0 +1,9 @@
+
+$(function(){
+    $('#fullpage').fullpage({
+		anchors:['section1', 'section2', 'section3', 'section4', 'section5'],
+		autoScrolling:true,
+		scrollHorizontally: true,
+        navigation: true,
+	});
+})
