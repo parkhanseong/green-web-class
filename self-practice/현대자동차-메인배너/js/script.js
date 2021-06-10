e902a6af1535d4fb67fcaa173c907e53b6419da3
parkhanseong <hs88610@gmail.com>
2021-06-10 19:06:32 +0900

    현대자동차 - 메인배너 클론

---
+++ self-practice/현대자동차-메인배너/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.main__banner').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+});
