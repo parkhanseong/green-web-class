10bcae0d773016bfa32a7e6bd175819a6ccf85e8
parkhanseong <hs88610@gmail.com>
2021-06-10 19:34:28 +0900

    슬릭 슬라이더 커스텀 추가

---
+++ self-practice/슬릭슬라이더-커스텀/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider__custom').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
+    });
+});
